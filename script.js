fa2a531038a00f06210023187ec60011fd708517
George Brikae <georgebrikae@Georges-MacBook-Pro.local>
2026-09-16 15:46:02 -0400

    Use prayer photo and visible Request A prayer label

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const arabicTranslations = {
   "Saint Thekla": "القديسة تقلا",
   "Saint Thekla Melkite Catholic Community": "جماعة القديسة تقلا الملكية الكاثوليكية",
   "Home": "الرئيسية",
-  "Request Prayer": "طلب صلاة",
+  "Request A prayer": "طلب صلاة",
   "Calendar": "التقويم",
   "First Time?": "أول زيارة؟",
   "Bulletins": "النشرات",
